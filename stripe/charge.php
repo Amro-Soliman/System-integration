c52f5478d63b1096a9e15613a5c65236106c08ef
Amro Soliman <amro.soliman@protonmail.com>
2019-05-30 18:34:49 +0200

    Getting ahead with the project

--- stripe/charge.php
+++ stripe/charge.php
@@ -5,9 +5,92 @@ require_once('../vendor/autoload.php');
 
 // Get the token from the JS script
 $token = $_POST['stripeToken'];
-$charge = \Stripe\Charge::create(['amount' => 200000, 'currency' => 'usd', 'source' => $token]);
-echo $charge;
-\Stripe\Invoice::retrieve('ch_1EfMDwEX9zYTdRRzlZFftu8C');
+$charge = \Stripe\Charge::create(['amount' => 200000, 'currency' => 'USD', 'source' => $token]);
+if($charge){
+    echo 'You have paid' . ' ' . $charge->amount .' ' . $charge->currency . '</br></br>';
+}else{'Everything is NOT awesome';}
+
+$filename = '../upload-file/uploaded_files/books.csv';
+// The nested array to hold all the arrays
+$books = [];
+$books[] = ['ISBN', 'Namn', 'Beskrivning'];
+// Open the file for reading
+if ($file_handle = fopen($filename, 'r')) {
+    // Read one line from the csv file, use comma as separator
+    while ($data = fgetcsv($file_handle)) {
+      
+        for($i=0; $i <= sizeof($data) - 1; $i++) {
+
+        
+        $books[] = fill_book($data[$i]);
+        }
+    }
+    // Close the file
+    fclose($file_handle);
+}
+// Display the code in a readable format
+//var_dump($books);
+if ($books) {
+    $filename = 'new_books.csv';
+    $file_to_write = fopen($filename, 'w');
+    $everything_is_awesome = true;
+    foreach ($books as $book) {
+//        $book = fill_book($book[0]);
+        //var_dump($book);
+        $everything_is_awesome = $everything_is_awesome && fputcsv($file_to_write, $book);
+    }
+    fclose($file_to_write);
+    if ($everything_is_awesome && $charge) {
+        echo '<a href="' . $filename . '">Download file</a></br></br>';
+    } else {
+        echo 'Everything is NOT awesome';
+         
+    }
+}
+function fill_book($isbn)
+{
+    $curl = curl_init();
+  
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => "http://apiprojekt.mistert.se/APIcourse/Pages/query_response.php?table=Books&apikey=5ced6b2775afb&ISBN=$isbn",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    
+    ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    
+    curl_close($curl);
+        $response = json_decode($response, true); //because of true, it's in an array
+        // var_dump($response['results'][0]['Namn']);
+         $book = [];
+         $book[0] = $isbn;
+         $book[1] = $response['results'][0]['Namn'];
+         $book[2] = $response['results'][0]['Beskrivning'];
+         return $book;
+       print_r($book);
+       }
+
+if($charge){
+    echo "<h1> Your Books </h1>";
+    foreach($books as $value){
+        
+        
+        
+        echo '<b>' .$value[0] .'</b>' . '          ' . '<b>' .  $value[1] . '</b>' . '           '. '<b>' .  $value[2]  . '</b> </br>';
+    
+}
+
+
+}
+
+
+
+
+
+
+
 
 // This is a 20.00 charge in SEK.
 // Charging a Customer
--- upload-file/index.php
+++ upload-file/index.php
@@ -7,49 +7,85 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Upload files</title>
     <style>
-#upload {
-        height: 167px;
-        width: 400px;
-        background-color: burlywood;
-        padding: 26px;
-        position: absolute;
-        top: 56px;
-        left: 428px;
+    .StripeElement {
+    background-color: white;
+    padding: 8px 12px;
+    border-radius: 4px;
+    border: 1px solid transparent;
+    box-shadow: 0 1px 3px 0 #e6ebf1;
+    -webkit-transition: box-shadow 150ms ease;
+    transition: box-shadow 150ms ease;
+}
+.StripeElement--focus {
+    box-shadow: 0 1px 3px 0 #cfd7df;
+}
+.StripeElement--invalid {
+    border-color: #fa755a;
+}
+.StripeElement--webkit-autofill {
+    background-color: #fefde5 !important;
+}
 
+#upload {
+    height: 167px;
+    width: 495px;
+    background-color: cadetblue;
+    padding: 26px;
+    position: absolute;
+    top: 56px;
+    left: 361px;
 
 }
-#form2 {
-        height: 167px;
-        width: 327px;
-        background-color: burlywood;
-        margin-left: 421px;
-        padding: 26px;
-        position: absolute;
-        top: 312px;
-        left: 249px;
+#payment-form {
+    height: 220px;
+    width: 327px;
+    background-color: cadetblue;
+    color: aliceblue;
+    margin-left: 421px;
+    padding: 26px;
+    position: absolute;
+    top: 281px;
+    left: 188px;
 }
 
 #listItems {
-        width: 300px;
-        height: auto;
-        background-color: cadetblue;
-        color: aliceblue;
-        padding: 12px;
-        list-style: none;
-        position: absolute;
-        top: 333px;
-        left: 338px;
+    width: 300px;
+    height: auto;
+    background-color: cadetblue;
+    color: aliceblue;
+    padding: 12px;
+    list-style: none;
+    position: absolute;
+    top: 306px;
+    left: 281px;
 }
 #header {
-        width: 320px;
-        height: 33px;
+    width: 320px;
+    height: 33px;
+    background-color: coral;
+    color: aliceblue;
+    position: absolute;
+    top: 266px;
+    left: 281px;
+    line-height: 1.5em;
+    padding: 2px;
+}
+#submit {
+        width: 177px;
+        height: 49px;
         background-color: coral;
-        color: aliceblue;
-        position: absolute;
-        top: 296px;
-        left: 338px;
-        line-height: 1.5em;
-        padding: 2px;
+        color: azure;
+}
+#fileButton {
+    color: whitesmoke;
+    height: 40px;
+}
+#btn {
+    height: 38px;
+    width: 165px;
+    background-color: coral;
+    color: azure;
+    margin-left: 77px;
 }
 
          </style>
@@ -58,10 +94,10 @@
 
 <form id='upload' method="post" enctype="multipart/form-data">
 
-Upload your CSV file:<br>
-<input type="file" name="books_file"><br>
+<h2 style='color:azure;'>Upload your CSV file: </h2>
+<input type="file" id='fileButton' name="books_file"><br><br><br>
 
-<input type="submit" name='submit' value="Upload file">
+<input type="submit" id='submit' name='submit' value="Upload file">
 </form>
 
 
@@ -87,7 +123,7 @@ $error = array();
         move_uploaded_file($_FILES['books_file']['tmp_name'], "$path");
 }
 
-$header = "<h2 id='header'>Here is your books</h2>";
+$header = "<h2 id='header'>Here is your ISBNs</h2>";
 $filename = 'uploaded_files/books.csv';
 
     if (!$fileType){
@@ -114,15 +150,45 @@ $filename = 'uploaded_files/books.csv';
 
     if($div){
 
-        $form = "<form id='form2' action='' method='' > ";
-        $form .= " Name: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='text' name='name'> </br> </br>";
-        $form .= "Adress: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='text' name='adress'> </br> </br>";
-        $form .= "Telephone nr: <input type='tel' name='tel'>";
+        $form =  "<form action='../stripe/charge.php' method='post' id='payment-form'>";
+        $form .= "Name: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='text' name='name'> </br> </br>";
+        $form .= "Adress: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='text' name='adress'> </br> </br>";
+        $form .= "Telephone nr: <input type='tel' name='tel'></br></br>";
+
+        $form .= "<div class='form-row'></br>";
+        $form .= "<label for='card-element'>Credit or debit card</label>";
+
+        $form .= "<div id='card-element'>";
+        // a Stripe Element will be inserted here.
+        $form .= "</div>";
+        //Used to display form errors
+        $form .= "<div id='card-errors'></div>";
+        $form .= "</div></br></br>";
+        $form .= "<button id='btn'>Submit Payment</button>";
+     
+        $form .= "</form>";
+
+       
 
         echo $form;
     }else   {$form ='';}
 ?>
 
+  <!-- The needed JS files -->
+<!-- JQUERY File -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Stripe JS -->
+<script src="https://js.stripe.com/v3/"></script>
+
+<!-- Your JS File -->
+<script src="../stripe/charge.js"></script>
+
+    
+    
+    
+  
+</form>
 
 </body>
 </html>
