8b16fff1f334aa7b4aad6fa301a06c94501cbfbf
Amro Soliman <amro.soliman@protonmail.com>
2019-05-30 12:55:40 +0200

    updated index page with some CSS and more PHP

--- upload-file/index.php
+++ upload-file/index.php
@@ -1,77 +1,128 @@
 <!DOCTYPE html>
+
 <html lang="en">
-<head>
+    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Upload files</title>
     <style>
-    form {
+#upload {
         height: 167px;
-    width: 400px;
-    background-color: burlywood;
-    margin-left: 421px;
-    margin-top: 151px;
-    padding: 26px;
+        width: 400px;
+        background-color: burlywood;
+        padding: 26px;
+        position: absolute;
+        top: 56px;
+        left: 428px;
+
 
 }
-    </style>
-</head>
+#form2 {
+        height: 167px;
+        width: 327px;
+        background-color: burlywood;
+        margin-left: 421px;
+        padding: 26px;
+        position: absolute;
+        top: 312px;
+        left: 249px;
+}
+
+#listItems {
+        width: 300px;
+        height: auto;
+        background-color: cadetblue;
+        color: aliceblue;
+        padding: 12px;
+        list-style: none;
+        position: absolute;
+        top: 333px;
+        left: 338px;
+}
+#header {
+        width: 320px;
+        height: 33px;
+        background-color: coral;
+        color: aliceblue;
+        position: absolute;
+        top: 296px;
+        left: 338px;
+        line-height: 1.5em;
+        padding: 2px;
+}
+
+         </style>
+    </head>
 <body>
 
-<form method="post" enctype="multipart/form-data">
+<form id='upload' method="post" enctype="multipart/form-data">
 
 Upload your CSV file:<br>
 <input type="file" name="books_file"><br>
 
-<input type="submit" value="Upload file">
+<input type="submit" name='submit' value="Upload file">
 </form>
 
 
 
-
-<pre>
 <?php
 
+
+$div = '';
 $error = array();
-if (isset($_FILES) && isset($_POST['submit'])) {
-    $fileType = $_FILES['books_file']['type'];
-    $check = true;}else{$fileType = '';}
+    if (isset($_FILES) && isset($_POST['submit'])) {
+        $fileType = $_FILES['books_file']['type'];
+        $div = TRUE;
+        $check = true;}else{$fileType = '';}
     if ($fileType !== 'application/octet-stream') {
         $check = false;
-    }else{
+        $div = FALSE;
+        echo 'we accept here just csv';
+            }else{
         $check = true;
-    }
-    if ($check) {
-        $date = date('Ymd_His');
-        $file_id = uniqid();
-        // $path = realpath('./') . '/uploaded_files/' . $file_id; // . '_' . $_FILES['books_file']['name'];
-        $path = realpath('./') . '\\uploaded_files\\' . $_FILES['books_file']['name'];
-        // $sql = "INSERT INTO files (file, file_name) VALUES ('$file_id', '$_FILES["books_file"]["name"]')";
-       
-        move_uploaded_file($_FILES['books_file']['tmp_name'], "$path");
-    }
-
-?>
-<h2>files</h2>
-<?php
-
-
-$filename = 'uploaded_files/books.csv';
-if ($file_handle = fopen($filename, 'r')) {
-    // Read one line from the csv file, use comma as separator
-    while ($data = fgetcsv($file_handle)) {
-      
-        for($i=0; $i <= sizeof($data) - 1; $i++) {
-
-        
-        echo $data[$i] . '</br>';
-        }
-    }
-    // Close the file
-    fclose($file_handle);
 }
+    if ($check) {
+        $path = realpath('./') . '\\uploaded_files\\' . $_FILES['books_file']['name'];
+        move_uploaded_file($_FILES['books_file']['tmp_name'], "$path");
+}
+
+$header = "<h2 id='header'>Here is your books</h2>";
+$filename = 'uploaded_files/books.csv';
+
+    if (!$fileType){
+        echo '';}elseif ($file_handle = fopen($filename, 'r')) {
+    
+        echo $header;
+        $div = '<ul id="listItems">';
+        // Read one line from the csv file, use comma as separator
+    while ($data = fgetcsv($file_handle)) {
+          
+    for($i=0; $i <= sizeof($data) - 1; $i++) {
+                
+        $div .= '<li>' . $data[$i] . '</li>';
+           
+}
+
+}
+        // Close the file
+    
+        fclose($file_handle);
+        $div .= '</ul>';
+        echo $div;
+}
+
+    if($div){
+
+        $form = "<form id='form2' action='' method='' > ";
+        $form .= " Name: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='text' name='name'> </br> </br>";
+        $form .= "Adress: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type='text' name='adress'> </br> </br>";
+        $form .= "Telephone nr: <input type='tel' name='tel'>";
+
+        echo $form;
+    }else   {$form ='';}
 ?>
+
 
 </body>
 </html>
